a00f907ba156df969dcaae59d7aea935a6010fb4
Chris Mowforth <chris.mowforth@hardpoint.dev>
2026-04-20 16:25:39 +0400

    avoid pooling buffer for varint encoding

--- src/encode.ts
+++ src/encode.ts
@@ -3,37 +3,17 @@ import { Transform, TransformCallback } from 'node:stream';
 
 export class Encoder extends Transform {
 
-  private pool = Buffer.allocUnsafe(10 * 1024);
-  private used = 0
-  private _destroyed: boolean;
-
   constructor() {
     super();
-    this._destroyed = false;
   }
 
   _transform(data: Buffer, enc: BufferEncoding, cb: TransformCallback): void {
-    if (this._destroyed) return cb();
+    const lengthBuffer = Buffer.alloc(10);
+    varint.encode(data.length, lengthBuffer, 0);
 
-    varint.encode(data.length, this.pool, this.used);
-    this.used += varint.encode.bytes!;
-
-    this.push(this.pool.subarray(this.used - varint.encode.bytes!, this.used));
+    this.push(lengthBuffer.subarray(0, varint.encode.bytes!));
     this.push(data);
 
-    if (this.pool.length - this.used < 100) {
-      this.pool = Buffer.allocUnsafe(10 * 1024);
-      this.used = 0;
-    }
-
     cb();
-  }
-
-  destroy(err?: Error | undefined): this {
-    if (this._destroyed) return this;
-    this._destroyed = true;
-    if (err) this.emit('error', err);
-    this.emit('close');
-    return this;
   }
 }
